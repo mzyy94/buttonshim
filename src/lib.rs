b7d39647bc9d78195ca9d73f42bd2bdb5001a19f
Yuki MIZUNO <mizuyuu0904@gmail.com>
2022-03-15 23:33:14 +0900

    Add channel event sender

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use rppal::i2c::I2c;
+use std::sync::mpsc::Sender;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::{Duration, Instant};
@@ -108,6 +109,28 @@ impl Led {
 }
 
 #[derive(Copy, Clone, Debug)]
+pub enum Button {
+    A(State),
+    B(State),
+    C(State),
+    D(State),
+    E(State),
+}
+
+impl Button {
+    fn from_index(i: usize, state: State) -> Option<Self> {
+        match i {
+            0 => Some(Button::A(state)),
+            1 => Some(Button::B(state)),
+            2 => Some(Button::C(state)),
+            3 => Some(Button::D(state)),
+            4 => Some(Button::E(state)),
+            _ => None,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
 pub enum State {
     Released,
     Pressed(Instant),
@@ -119,6 +142,7 @@ pub struct Buttons {
     i2c: Arc<Mutex<I2c>>,
     buttons: Arc<Mutex<Vec<State>>>,
     hold_threshold: Duration,
+    sender: Option<Sender<Button>>,
 }
 
 impl Buttons {
@@ -129,6 +153,7 @@ impl Buttons {
             i2c,
             buttons: Arc::new(Mutex::new(vec![State::Released; 5])),
             hold_threshold: Duration::from_secs(2),
+            sender: None,
         }
     }
 
@@ -191,12 +216,27 @@ impl Buttons {
         let i2c = Arc::clone(&self.i2c);
         let buttons = Arc::clone(&self.buttons);
         let hold_threshold = self.hold_threshold;
+        let sender = self.sender.as_ref().map(Sender::clone);
 
         thread::spawn(move || loop {
             let current = buttons.lock().unwrap().clone();
-            *buttons.lock().unwrap() = Self::get_state(&i2c, current, hold_threshold);
+            let now = Self::get_state(&i2c, current.clone(), hold_threshold);
+            *buttons.lock().unwrap() = now.clone();
+            if let Some(ref sender) = sender {
+                for i in 0..5 {
+                    if current[i] != now[i] {
+                        if let Some(button) = Button::from_index(i, now[i]) {
+                            sender.send(button).unwrap();
+                        }
+                    }
+                }
+            }
 
             thread::sleep(interval)
         });
+    }
+
+    pub fn set_sender(&mut self, sender: Sender<Button>) {
+        self.sender = Some(sender);
     }
 }
